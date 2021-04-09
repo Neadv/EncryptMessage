161b6744f5872d94413d8a03bf3b0c8cbab408a0
Vlad Krinitskiy <38957031+Neadekvashka@users.noreply.github.com>
2021-04-09 20:20:11 +0300

    Add initial migration

---
+++ EncryptMessage/Migrations/20210409171659_Initial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EncryptMessage.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(6)", nullable: false),
+                    Value = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    IV = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
