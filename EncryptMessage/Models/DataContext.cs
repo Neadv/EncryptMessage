7471977370df94cc7d7a8bfc78e010bd76a29093
Vlad Krinitskiy <38957031+Neadekvashka@users.noreply.github.com>
2021-04-09 17:15:56 +0300

    Add database context

---
+++ EncryptMessage/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptMessage.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DbContext> options) : base(options) { }
+    }
+}
--- EncryptMessage/Startup.cs
+++ EncryptMessage/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EncryptMessage.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace EncryptMessage
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<DataContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
